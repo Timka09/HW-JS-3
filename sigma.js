203708b2c7c8626c55c1719ac91e6cc4a7a981bb
Timka09 <tymofij.i.naumenko@gmail.com>
2024-09-27 22:12:33 +0300

    preveeeeeeeeeet

---
+++ sigma.js
@@ -0,0 +1,19 @@
+// Task 1
+const result = 5 + 5 + '5';
+console.log(result)
+// Task 2 
+const email = "tymofij.i.naumenko@gmail.com";
+const emailCheck = email.includes("@");
+const symbolCheck = email.length;
+console.log(symbolCheck)
+console.log(emailCheck)
+// Task 3
+const my = "my";
+const name = "name";
+const is = "is";
+const fullName = my + " " + name + " " + is + " ";
+console.log(fullName + "Viktor");
+// Task 4
+const userName = prompt("Type here name pls!!!!");
+const payment = " 300 гривень!!!";
+alert(userName + " До сплати!!!!" + payment);
